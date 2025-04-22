5e39bad90b4d6e7fd53f2c05b6a227fbdb9cfc63
freshduer <582544712@qq.com>
2025-04-22 08:31:42 +0800

    feat(add oversize dataset method)

--- torchrec/datasets/criteo.py
+++ torchrec/datasets/criteo.py
@@ -777,18 +777,37 @@ class InMemoryBinaryCriteoIterDataPipe(IterableDataset):
         val_ratio = args.val_ratio
         test_start_ratio = args.test_start_ratio
         test_end_ratio = args.test_end_ratio
-        faiss_ratio = args.faiss_ratio
         
         continue_train_start_index = int(len_d0 * continue_train_start_ratio)
         train_end_index = int(len_d0 * train_ratio)
-        faiss_start_index = int(len_d0 * faiss_ratio)
         test_start_index = int(len_d0 * test_start_ratio)
         test_end_index = int(len_d0 * test_end_ratio)
         # test_start_index = int(len_d0 * (1 - test_ratio))
         if stage == "train":
-            self.dense_arrs[0] = self.dense_arrs[0][:train_end_index, :]
-            self.sparse_arrs[0] = self.sparse_arrs[0][:train_end_index, :]
-            self.labels_arrs[0] = self.labels_arrs[0][:train_end_index, :]
+            total_len = len(self.labels_arrs[0])
+            repeat_times = int(train_ratio)           # 整数部分，完整数据重复几次
+            remain_ratio = train_ratio - repeat_times # 小数部分，再训练一部分
+
+            # 收集所有需要训练的切片
+            dense_list = []
+            sparse_list = []
+            labels_list = []
+
+            for _ in range(repeat_times):
+                dense_list.append(self.dense_arrs[0])
+                sparse_list.append(self.sparse_arrs[0])
+                labels_list.append(self.labels_arrs[0])
+
+            if remain_ratio > 0:
+                end_idx = int(total_len * remain_ratio)
+                dense_list.append(self.dense_arrs[0][:end_idx, :])
+                sparse_list.append(self.sparse_arrs[0][:end_idx, :])
+                labels_list.append(self.labels_arrs[0][:end_idx, :])
+
+            # 拼接所有片段
+            self.dense_arrs[0] = np.concatenate(dense_list, axis=0)
+            self.sparse_arrs[0] = np.concatenate(sparse_list, axis=0)
+            self.labels_arrs[0] = np.concatenate(labels_list, axis=0)
         elif stage == "continue_train":
             self.dense_arrs[0] = self.dense_arrs[0][continue_train_start_index:train_end_index, :]
             self.sparse_arrs[0] = self.sparse_arrs[0][continue_train_start_index:train_end_index, :]
@@ -801,10 +820,6 @@ class InMemoryBinaryCriteoIterDataPipe(IterableDataset):
             self.dense_arrs[0] = self.dense_arrs[0][test_start_index:test_end_index, :]
             self.sparse_arrs[0] = self.sparse_arrs[0][test_start_index:test_end_index, :]
             self.labels_arrs[0] = self.labels_arrs[0][test_start_index:test_end_index, :]
-        elif stage == "faiss":
-            self.dense_arrs[0] = self.dense_arrs[0][train_end_index:faiss_start_index, :]
-            self.sparse_arrs[0] = self.sparse_arrs[0][train_end_index:faiss_start_index, :]
-            self.labels_arrs[0] = self.labels_arrs[0][train_end_index:faiss_start_index, :]
         # When mmap_mode is enabled, sparse features are hashed when
         # samples are batched in def __iter__. Otherwise, the dataset has been
         # preloaded with sparse features hashed in the preload stage, here:
